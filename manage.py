ff58932d77bf767135593ef722fb915105c54d01
blyap <blypik@gmail.com>
2014-08-19 14:41:49 +0400

    new2

--- manage.py
+++ manage.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 import os
-COV = None # test1
+COV = None
 if os.environ.get('FLASK_COVERAGE'):
     import coverage
     COV = coverage.coverage(branch=True, include='app/*')
